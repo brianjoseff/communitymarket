642144390c42924141896f81defa88047b4b951a
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2013-01-24 11:12:30 -0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Communitymarket::Application.config.secret_token = '755e71171243bd599eef8215d16bf1e9ac39cd4f4b1cd33e3b89483393c6bb788c0b64d4bbe57da122dc50dc5b794020160cef2ec46c0cd10c74ba52be7f6cae'
