553a411e614b5f82ed38524456711ddcf4333269
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2013-02-27 13:40:26 -0800

    javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -64,33 +64,33 @@ $(document).ready(function () {
     title: 'Edit',
     modal: true,
     draggable: false,
-	buttons: {
-	            "Save": function () {
-	                    $("#new_transaction").submit(function () {
-						    var valuesToSubmit = $(this).serialize();
-						    $.ajax({
-						        url: $(this).attr('action'), //sumbits it to the given url of the form
-						        data: valuesToSubmit,
-								type: 'POST',
-						        dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
-						    }).success(function (json) {
-						        //act on result.
-						    });
-						    return false;
-						});
-	                    $('#buy_modal').dialog( "close" );
-	            },
-	            Cancel: function () {
-	                $( this ).dialog( "close" );
-	            }
-	        }
+ buttons: {
+             "Save": function () {
+                     $("#new_transaction").submit(function () {
+                         var valuesToSubmit = $(this).serialize();
+                         $.ajax({
+                             url: $(this).attr('action'), //sumbits it to the given url of the form
+                             data: valuesToSubmit,
+                             type: 'POST',
+                             dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
+                         }).success(function (json) {
+                             //act on result.
+                         });
+                         return false;
+                     });
+                     $('#buy_modal').dialog( "close" );
+             },
+             Cancel: function () {
+                 $( this ).dialog( "close" );
+             }
+         }
 
-	});
-	// $('.buy-button').bind('ajax:before', function() {
-	//         $(this).data('params', { post_id: 'post_feed_item.id', tier_id: 'post_feed_item.tier_id', price: 'post_feed_item.price' });
-	//     });
-	$('.buy-button').click(function () {
-	    $buy_dialog.dialog('open');
+ });
+ // $('.buy-button').bind('ajax:before', function() {
+ //         $(this).data('params', { post_id: 'post_feed_item.id', tier_id: 'post_feed_item.tier_id', price: 'post_feed_item.price' });
+ //     });
+ $('.buy-button').click(function () {
+     $buy_dialog.dialog('open');
   });
 });
 
