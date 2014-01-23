38b85cf1a5c3821ae6f62bf42271ac7ec10f5dfc
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-01-22 21:45:03 -0500

    fixed search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,7 +12,7 @@ class SearchController < ApplicationController
     # end
     @q2 = Post.search({:title_cont => params[:q][:name_cont]})
     #@q2 = Post.search(params[:q])
-    @posts_results = @q2.result
+    @posts_results = @q2.result.select{|x| x.active?}.sort { |x,y| y.created_at <=> x.created_at }
     @posts_results = Kaminari.paginate_array(@posts_results).page(params[:page]).per(35)
     @post_count = @posts_results.count
     @posts = @posts_results
