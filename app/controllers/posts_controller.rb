a7a5aef897e5cfd7ca462d76325da3daa380e390
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-01-22 22:18:54 -0500

    fixed inactive posts in recommended bar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
     else
       @user = current_user
     end
-    @random_posts = Post.limit(7).order("RANDOM()") - @user.posts
+    @random_posts = Post.limit(7).order("RANDOM()").select{|x| x.active?} - @user.posts
     @random_posts.delete(@post)
     respond_to do |format|
       format.html # show.html.erb
