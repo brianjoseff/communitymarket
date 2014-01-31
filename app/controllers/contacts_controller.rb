853da4a3dcfa03fb576a713bbcea0cc315a92000
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-01-31 12:51:59 -0500

    contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,9 @@
 class ContactsController < ApplicationController
   def new
     @content = params[:content]
+    @email = params[:email]
     @user = current_user
-    ContactMailer.vox(@user, @content).deliver
+    ContactMailer.vox(@user, @content, @email).deliver
     redirect_to about_path, :notice => "Thanks for the message"
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default :from => "user-input@peopleandstuff.com"
   
-  def vox(user, content)
+  def vox(user, content, email)
     @content = content
     @user = user
-    mail(:to => ENV["GMAIL_USERNAME"], :subject => "User Input", :reply_to => @user ? @user.email : "")
+    mail(:to => ENV["GMAIL_USERNAME"], :subject => "User Input", :reply_to => @user ? @user.email : email)
   end
 end
