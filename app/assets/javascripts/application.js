db1eae1687cf122d8093df3e81ccebfac6807f94
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2014-08-24 23:13:08 -0700

    post form and completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,7 +79,7 @@ function remove_fields(link) {
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).parent().before(content.replace(regexp, new_id));
+  $(link).parent().after(content.replace(regexp, new_id));
 }
 // document.ready(function() {
 //   var flipText, flipUp, index, interval, maxIndex;
