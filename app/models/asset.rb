ec55b8f94f52de7c0a862cd2bc9d916749e285fa
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2015-01-13 10:58:34 -0800

    fixed bucket

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -6,7 +6,7 @@ class Asset < ActiveRecord::Base
   #settings for Paperclip gem that attaches images
   has_attached_file :image, :styles => { :normal => "100%",:small => "100 x100>",:medium => "200x200>", :thumb => "50x50>", :mini => "25x25>" },
                             :storage => :s3,
-                            bucket: 'zounds-dev',
+                            bucket: 'zounds-pro',
                             #:s3_credentials => Proc.new{|a| Rails.env.production? ? a.instance.prod_s3_credentials :  a.instance.s3_credentials}, 
                             :s3_credentials => "#{Rails.root}/config/s3.yml", 
                             :path => "/:attachment/:id/:style/:filename"
